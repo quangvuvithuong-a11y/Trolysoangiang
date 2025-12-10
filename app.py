c452002c2359c2d661b71a612f5f2775335c26c7
quangvuvithuong-a11y <quangvuvithuong@gmail.com>
2025-12-10 11:14:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Dương Doãn Vũ - Trường Tiểu học Vĩ Thượng - ĐT: 0964064118</div>", unsafe_allow_html=True)
