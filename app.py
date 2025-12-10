d54158386e13523de61a65d86c4b912c05d7e1de
quangvuvithuong-a11y <quangvuvithuong@gmail.com>
2025-12-10 10:54:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Dương Doãn Vũ - Trường Tiểu học Vĩ Thượng - ĐT: 0964 064 118</p>
 </div>
 """, unsafe_allow_html=True)
 
